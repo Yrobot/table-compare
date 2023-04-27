d72c5b4d28f7618a5c20e0e58c4ff4e77781d613
Yrobot <Yrobot@qq.com>
2023-04-27 22:33:04 +0800

    update compare logic

--- index.ts
+++ index.ts
@@ -1,53 +1,57 @@
 import * as Excel from "exceljs";
-import * as csv from "csv-parser";
 import * as fs from "fs";
 
-const getXlsxTitles = async (filePath) => {
+const getXlsxSheet = async (filePath) => {
   const workbook = new Excel.Workbook();
-  const worksheet = (await workbook.xlsx.readFile(filePath)).getWorksheet(1);
-  return worksheet.getColumn(5).values;
+  const worksheet = (await workbook.xlsx.readFile(filePath)).worksheets[0];
+  return worksheet;
+  // return worksheet.getColumn(column).values;
 };
 
-const getCsvTitles = async (filePath) =>
-  new Promise((resolve, reject) => {
-    let result: string[] = [];
-    fs.createReadStream(filePath)
-      .pipe(csv({ separator: "\t" }))
-      .on("data", (d) => {
-        result.push(d.Title);
-      })
-      .on("end", () => {
-        resolve(result);
-      });
-  });
+const writeXlsxSheet = async (filePath: string, data: any[]) => {
+  const workbook = new Excel.Workbook();
+  const worksheet = workbook.addWorksheet();
+  worksheet.addRows(data);
+  return await workbook.xlsx.writeFile(filePath);
+};
 
-const config: { path: string; getTitles: Function }[] = [
+const config: { path: string; getSheet: Function }[] = [
   {
     path: "db/OSS1.0.xlsx",
-    getTitles: async (path) => getXlsxTitles(path).then((arr) => arr.slice(2)),
+    getSheet: getXlsxSheet,
   },
   {
-    path: "db/OSS2.0.csv",
-    getTitles: getCsvTitles,
+    path: "db/OSS2.0.xlsx",
+    getSheet: getXlsxSheet,
   },
 ];
 
+const formatTitle = (title: string) => title.trim().toLocaleLowerCase();
+
 (async () => {
-  const result: string[] = [];
+  const result: Object[] = [];
   const map = {};
-  const titles1 = await config[0].getTitles(config[0].path);
-  const titles2 = await config[1].getTitles(config[1].path);
+  const sheet1 = await config[0].getSheet(config[0].path);
+  const sheet2 = await config[1].getSheet(config[1].path);
+
+  const titles1 = sheet1.getColumn(5).values.slice(2);
 
   titles1.forEach((title, i) => {
-    map[title] = true;
+    map[formatTitle(title)] = true;
   });
-  titles2.forEach((title, i) => {
-    if (!map[title]) {
-      result.push(title);
-    } else {
+
+  sheet2.eachRow((row, i) => {
+    if (i === 1) {
+      result.push(row.values);
+      return;
+    }
+    const title = row.values[3];
+    if (!map[formatTitle(title)]) {
+      result.push(row.values);
     }
   });
-  await fs.writeFileSync("result.txt", result.join("\n"));
 
-  console.log(titles1.length, titles2.length, result.length);
+  console.log(sheet1.rowCount - 1, sheet2.rowCount - 1, result.length - 1);
+
+  await writeXlsxSheet("output.xlsx", result);
 })();
